e888224cdddb0ac35de250c25ca9cf8151bf4449
Kolachow <kolas3000@gmail.com>
2018-12-23 11:14:29 +0100

    Empty project - ready to write

---
+++ src/main/java/pl/mkolasinski/Quest1/Quest1Application.java
@@ -0,0 +1,14 @@
+package pl.mkolasinski.Quest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Quest1Application.class, args);
+	}
+
+}
+
